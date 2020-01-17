d759598151a50163008f469fc7faf0caa2ed3513
joel <joelcote68@gmail.com>
2020-01-17 11:01:50 -0600

    added variables

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
 module "bigtable" {
   source          = "./modules/bigtable"
+  cluster_name = var.cluster_name
+  display_name = var.display_name
+  project_id   = var.project_id
+  zone         = var.zone
 }
