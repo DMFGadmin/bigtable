ea016113095f1e053a98c23ed15d0f8abdc5fb6b
joel <joelcote68@gmail.com>
2020-01-23 15:12:08 -0600

    cleaning up

--- main.tf
+++ main.tf
@@ -1,7 +1,26 @@
-module "bigtable" {
-  source          = "./modules/bigtable"
-  cluster_name = var.cluster_name
+resource "google_project_service" "enable_bigtable_api" {
+  project = var.project_id
+  service = "bigtableadmin.googleapis.com"
+}
+
+
+resource "google_bigtable_instance" "development-instance" {
+  name          = "tf-instance"
+  instance_type = "DEVELOPMENT"
   display_name = var.display_name
-  project_id   = var.project_id
-  zone         = var.zone
+  project = var.project_id
+
+  cluster {
+    cluster_id   = var.cluster_name
+    zone         = var.zone
+    storage_type = "HDD"
+  }
+  depends_on = [google_project_service.enable_bigtable_api]
+}
+
+resource "google_bigtable_table" "table" {
+  name          = "tf-table"
+  project = var.project_id
+  instance_name = google_bigtable_instance.development-instance.name
+  split_keys    = ["a", "b", "c"]
 }
