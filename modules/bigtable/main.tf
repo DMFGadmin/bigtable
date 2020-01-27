6975ffdeac079e9c4f6b1a19260977e5c2555b82
joel <joelcote68@gmail.com>
2020-01-27 15:09:16 -0600

    test

--- modules/bigtable/main.tf
+++ modules/bigtable/main.tf
@@ -1,13 +1,3 @@
-data "terraform_remote_state" "project-and-networks" {
-  backend = "remote"
-  config = {
-    organization = "AFRLDigitalMFG"
-    workspaces = {
-      name = "shared_vpc_projects"
-    }
-  }
-}
-
 //resource "google_project_service" "enable_bigtable_api" {
 //  project = data.terraform_remote_state.project-and-networks.outputs.afrl-big-data-project-id
 //  service = "bigtableadmin.googleapis.com"
