a6430bd9f0f4452831ebc9766a6c73317b489ffc
joel <joelcote68@gmail.com>
2020-01-27 15:06:24 -0600

    zzzzzzz

--- modules/bigtable/main.tf
+++ modules/bigtable/main.tf
@@ -18,7 +18,7 @@ resource "google_bigtable_instance" "development-instance" {
   name          = "tf-instance"
   instance_type = "DEVELOPMENT"
   display_name = var.display_name
-  project = var.project_id
+  project = "projects/${var.project_id}"
 
   cluster {
     cluster_id   = var.cluster_name
@@ -30,7 +30,7 @@ resource "google_bigtable_instance" "development-instance" {
 
 resource "google_bigtable_table" "table" {
   name          = "tf-table"
-  project = var.project_id
+  project = "projects/${var.project_id}"
   instance_name = google_bigtable_instance.development-instance.name
   split_keys    = ["a", "b", "c"]
 }
